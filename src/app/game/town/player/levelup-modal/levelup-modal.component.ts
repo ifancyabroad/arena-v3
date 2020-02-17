c3ef09a85ed3ea24e0e62c6d9a235d89dcee3693
Edgar Nightingale <edgar.nightingale@gfm.co.uk>
2020-02-17 11:21:30 +0000

    Fix build errors

--- src/app/game/town/player/levelup-modal/levelup-modal.component.ts
+++ src/app/game/town/player/levelup-modal/levelup-modal.component.ts
@@ -4,6 +4,7 @@ import { Player } from 'src/app/shared/classes/player';
 import { BaseStats } from 'src/app/shared/interfaces/class';
 import Utils from '../../../../shared/utils';
 import { KeyValue } from '@angular/common';
+import { StatData, Config } from 'src/app/shared/config';
 
 interface DialogData {
   player: Player;
@@ -15,16 +16,19 @@ interface DialogData {
   styleUrls: ['./levelup-modal.component.scss']
 })
 export class LevelupModalComponent implements OnInit {
+  stats: StatData[];
   tempStats: BaseStats;
   startingTotal: number;
   startingPoints: number;
 
   constructor(
     public dialogRef: MatDialogRef<LevelupModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private config: Config
   ) { }
 
   ngOnInit() {
+    this.stats = this.config.stats.filter(stat => stat.type === 'main');
     this.tempStats = Utils.deepCopyFunction(this.data.player.getBaseStats());
     this.startingTotal = Object.values(this.tempStats).reduce((a: number, b: number) => a + b, 0);
     this.startingPoints = this.data.player.skillPoints;
